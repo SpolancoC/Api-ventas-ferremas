401530ff5710213cef5c3d54e9df76dd6191da79
Fenixssj <ale.lagosp@duocuc.cl>
2025-05-13 20:04:48 -0400

    conexión BD localhost

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use('/api/ventas', ventasRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}/api/ventas/1`);
 });
